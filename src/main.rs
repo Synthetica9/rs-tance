f0c4e90c1ab228a2a4f920f147ff6517611ffc8d
Patrick Hilhorst <git@hilhorst.be>
2019-06-06 23:58:04 +0200

    Some more work

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,15 @@
 extern crate lazy_static;
 extern crate primes;
 extern crate rug;
+extern crate ctrlc;
 
+use std::time::Instant;
 use crate::rug::ops::Pow;
 use primes::PrimeSet;
 use rug::Integer;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::env;
 
 type SmallInt = u8;
 type LargeInt = Integer;
@@ -79,17 +84,17 @@ lazy_static! {
   static ref PRIMES : Vec<SmallInt> = PrimeSet::new()
     .iter()
     .map(|i| i as SmallInt)
-    .take_while(|i| i < &BASE)
+    .take_while(|i| i <= &BASE)
     .collect();
-  // static ref PRIMES : Vec<SmallInt> = vec![2, 3, 7];
 }
 
 fn digits(x: LargeInt) -> Vec<SmallInt> {
     let mut vec = Vec::new();
     let mut remaining = x.clone();
     while remaining > 0 {
-        vec.push(large_to_small(remaining.clone() % small_to_large(BASE)));
-        remaining /= small_to_large(BASE);
+        let mut quot = small_to_large(BASE);
+        LargeInt::div_rem_mut(&mut remaining, &mut quot);
+        vec.push(large_to_small(quot));
     }
     vec.reverse();
     return vec;
@@ -127,14 +132,22 @@ fn factor(x: SmallInt) -> Bins {
     bins
 }
 
-fn assemble(xs: Bins) -> LargeInt {
+lazy_static! {
+    static ref BASE_FACTORS : Bins = factor(BASE);
+}
+
+fn divides(xs: &Bins, ys: &Bins) -> bool {
+    xs.iter().zip(ys.iter()).all(|(a, b)| a >= b)
+}
+
+fn assemble(xs: &Bins) -> LargeInt {
     let mut result = small_to_large(0);
     let mut mult = small_to_large(1);
 
     let mut bins = xs.clone();
     for i in (2..BASE).rev() {
         let factors = factor(i);
-        while bins.iter().zip(factors.iter()).all(|(a, b)| a >= b) {
+        while divides(&bins, &factors) {
             result += mult.clone() * small_to_large(i);
             mult *= small_to_large(BASE);
             for (j, val) in factors.iter().enumerate() {
@@ -145,46 +158,94 @@ fn assemble(xs: Bins) -> LargeInt {
     result
 }
 
+fn can_be_made(x: LargeInt) -> bool {
+    let mut remaining = x.clone();
+    for p in PRIMES.iter() {
+        remaining.remove_factor_mut(&small_to_large(*p));
+    }
+    return remaining == 1;
+}
+
 fn main() {
-    let mut n = 1;
+    println!("Starting");
+
+    let running = Arc::new(AtomicBool::new(true));
+    {
+        let r = running.clone();
+
+        ctrlc::set_handler(move || {
+            r.store(false, Ordering::SeqCst);
+        }).expect("Error setting Ctrl-C handler");
+    }
+
     let mut max_res = 0;
     let mut assembled_max = small_to_large(0);
     let mut iterations = 0;
-    'outer: loop {
-        println!("Starting new iteration: {}", n);
+    let mut discriminations = 0;
 
+    let now = Instant::now();
+
+    let break_on = {
+        let argv : Vec<String> = env::args().collect();
+        match argv.len() {
+            1 => !0,
+            2 => argv[1].parse::<usize>().unwrap(),
+            _ => panic!("at the disco"),
+        }
+    };
+
+    let mut n = 1;
+    'outer: loop {
         for bins in StarsBars::from(n) {
             iterations += 1;
+
+            // Like the 5 and 2 example, will get canceld out quickly, so skip immediately.
+            // Achieves a huge speedup, ~4x
+            if divides(&bins, &BASE_FACTORS) {
+                discriminations += 1;
+                continue;
+            }
+
             let mut a = small_to_large(1);
             for (exponent, base) in bins.iter().zip(PRIMES.iter()) {
                 a *= small_to_large(*base).pow(*exponent as u32);
             }
 
-            // if (a > milestone) {
-            //   println!("New milestone! {} {}", milestone_exponent, a);
-            //   milestone *= 10;
-            //   milestone_exponent += 1;
-            // }
-
-            let res = resistance(a);
+            let res = resistance(a) + 1; // One pass is done above.
             if res >= max_res {
-                let assembled = assemble(bins);
+                let assembled = assemble(&bins);
+                let native_assembled = assembled.to_string_radix(BASE as i32);
 
+                if can_be_made(assembled.clone()) {
+                    println!("Promising: {}", assembled);
+                }
                 if res > max_res {
+                    println!("Found new max!          {}\t{} ({})", res, native_assembled, assembled);
                     max_res = res;
-                    println!("Found new max!          {}\t{}", res, assembled);
                     assembled_max = assembled;
                 } else if assembled < assembled_max {
-                    println!("Found better assembly:  {}\t{}", res, assembled);
+                    println!("Found better assembly:  {}\t{} ({})", res, native_assembled, assembled);
                     assembled_max = assembled;
                 }
+
             }
+            if !running.load(Ordering::SeqCst) {
+                break 'outer;
+            };
+        }
+        // break if we're no longer running
+        if n == break_on {
+            break 'outer;
         }
         n += 1;
-
-        if n > 60 {
-            break;
-        }
     }
-    println!("Tested {} candidates.", iterations)
+
+    let duration = now.elapsed();
+    println!();
+    println!("Ran for {} cycles", n);
+    println!("Tested {} candidates.", iterations);
+    println!("Discriminated: {} ({:.2}%)", discriminations, (100. * discriminations as f64)/ (iterations as f64));
+    println!("Total: {}ms", duration.as_millis());
+    println!("Per candidate: {}ns", duration.as_nanos()/iterations);
+    println!("Per nondiscriminated candidate: {}ns", duration.as_nanos()/(iterations - discriminations));
 }
